3b31f7874a37a61d00d064c7b63f5aaf917f105d
陈云云 <hljyunxi@126.com>
2015-01-21 20:50:13 +0800

    Update url.cpp

--- url.cpp
+++ url.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 void ParamsStr2Map(
-        map<string, string>& key2value, const char* str, bool decode_uri)
+        map<string, string>& key2value, const char* str)
 {
     if (0 == str || 0 == *str) return;
 
@@ -40,16 +40,13 @@ void ParamsStr2Map(
 
         if (!key.empty())
         {
-            if (decode_uri)
-                key2value[DecodeUri(key, ktmp)] = DecodeUri(value, vtmp);
-            else
-                key2value[key] = value;
+             key2value[key] = value;
         }
     }
 }
 
 void ParamsMap2Str(
-        string& str, const map<string, string>& key2value, bool encode_uri)
+        string& str, const map<string, string>& key2value)
 {
     string ktmp, vtmp;
     bool firstone = true;
@@ -61,67 +58,6 @@ void ParamsMap2Str(
     for (; it != key2value.end(); ++it)
     {
         if (!firstone) str.push_back('&');
-        firstone = false;
-
-        if (encode_uri)
-            str.append(EncodeUri(it->first, ktmp)).append("=")
-                    .append(EncodeUri(it->second, vtmp));
-        else
-            str.append(it->first).append("=").append(it->second);
-    }
-}
-
-void CookiesStr2Map(map<string, string>& key2value, const char* str)
-{
-    if (0 == str || 0 == *str) return;
-
-    const char* ptr = str;
-    char c = 0;
-    string key, value;
-
-    key.reserve(512);
-    value.reserve(512);
-
-    while (*ptr != 0)
-    {
-        key.clear();
-        value.clear();
-
-        while ((c = *ptr) != 0 && (';' == c || ' ' == c)) ++ptr;
-
-        if (c != '=') // have key
-        {
-            const char* pkey = ptr;
-            while ((c = *ptr) != 0 && c != '=' && c != ';' && c != ' ') ++ptr;
-            if (ptr != pkey) key.assign(pkey, ptr - pkey);
-            while ((c = *ptr) != 0 && ' ' == c) ++ptr;
-        }
-
-        if ('=' == c)
-        {
-            while ((c = *ptr) != 0 && '=' == c || ' ' == c) ++ptr;
-            const char* pvalue = ptr;
-            while ((c = *ptr) != 0 && c != '=' && c != ';' && c != ' ') ++ptr;
-            if (ptr != pvalue) value.assign(pvalue, ptr - pvalue);
-        }
-
-        if (!key.empty()) key2value[key] = value;
-    }
-}
-
-void CookiesMap2Str(
-        string& str, const map<string, string>& key2value, bool add_space)
-{
-    bool firstone = true;
-
-    typeof(key2value.begin()) it = key2value.begin();
-    for (; it != key2value.end(); ++it)
-    {
-        if (!firstone)
-        {
-            str.push_back(';');
-            if (add_space) str.push_back(' ');
-        }
         firstone = false;
         str.append(it->first).append("=").append(it->second);
     }
